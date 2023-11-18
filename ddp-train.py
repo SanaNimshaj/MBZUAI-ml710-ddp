5270d4fd5752448884327656d38d216e8ac5ebd9
Sana K <sana.k@mbz>
2023-11-18 21:21:35 +0000

    sparsify and quantize committed

--- ddp-train.py
+++ ddp-train.py
@@ -5,11 +5,19 @@ import torch.nn as nn
 import torch.optim as optim
 import torch.distributed as dist
 import torch.multiprocessing as mp
-from torch.nn.parallel import DistributedDataParallel
+from torch.nn.parallel import DistributedDataParallel as DDP
 import torchvision
 
 from torch.distributed.algorithms.ddp_comm_hooks import powerSGD_hook
 from torch.distributed.algorithms.ddp_comm_hooks import quantization_hooks
+from torch.distributed.algorithms.ddp_comm_hooks import default_hooks
+
+import functools
+from torch.distributed.fsdp import FullyShardedDataParallel as FSDP
+from torch.distributed.fsdp.wrap import (   size_based_auto_wrap_policy,
+                                            enable_wrap,
+                                            wrap,
+                                        )
 
 import pandas as pd
 import time, datetime
@@ -18,18 +26,18 @@ import utils
 
 #*******************************************************************************
 
-approx_steps = 30000
+approx_steps = 200
 workers = 4
 batch_size = 64
 learning_rate =  0.001 #0.0001
 
 # Set the number of GPUs
-world_size = 2
-Method_Work = "ddp"  #  ddp / quantize / powersgd
+world_size = 4
+Method_Work = "quantize"  #  ddp / quantize / powersgd / fsdp / topk
 
 Rank_Adder = 0
 
-log_path = f"Logs/exp-{Method_Work}-{world_size}gpu/"
+log_path = f"Logs/exp03-{Method_Work}-{world_size}gpu-224im-Res18/"
 os.makedirs(log_path, exist_ok=True)
 
 os.environ['MASTER_ADDR'] = ADDR = 'localhost'
@@ -44,6 +52,7 @@ def get_data_stuffs():
         train=True,
         download=True,
         transform=torchvision.transforms.Compose([
+            torchvision.transforms.Resize((224, 224)),
             torchvision.transforms.ToTensor(),
             torchvision.transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
         ])
@@ -78,10 +87,82 @@ def get_model_stuffs(rank):
     model = torchvision.models.resnet18(weights=None)
     in_features = model.fc.in_features
     model.fc = nn.Linear(in_features, 10)
-
     model = model.to(rank)
-    dist_model = DistributedDataParallel(model, device_ids=[rank])
+
+    if Method_Work == "fsdp":
+        my_auto_wrap_policy = functools.partial(
+                size_based_auto_wrap_policy, min_num_params=100000 )
+        dist_model = FSDP(model, auto_wrap_policy=my_auto_wrap_policy)
+    else:
+        dist_model = DDP(model, device_ids=[rank])
+
     return dist_model, model
+
+#*******************************************************************************
+
+
+class TopKSparsificationState:
+    def __init__(self, process_group, k_ratio):
+        self.process_group = process_group
+        self.k_ratio = k_ratio
+
+    def sparsify_gradients(self, tensor):
+        flat_tensor = tensor#.flatten()
+        k_count = int( len(flat_tensor) // self.k_ratio )
+        abs_tensor = flat_tensor.abs()
+        topk_values, topk_indices = torch.topk(abs_tensor, k_count, sorted=False)
+        mask = torch.zeros_like(flat_tensor, dtype=torch.bool)
+        mask.scatter_(0, topk_indices, True)
+        sparsified_tensor = flat_tensor * mask
+        # sparsified_tensor = sparsified_tensor.reshape(tensor.shape)
+        return sparsified_tensor.to_sparse()
+
+def topk_sparsification_hook(state, bucket):
+    process_group = state.process_group
+    tensor = bucket.buffer()
+    sparsified_tensor = state.sparsify_gradients(tensor)
+    dist.all_reduce(sparsified_tensor, op=dist.ReduceOp.AVG,
+                    group=process_group)
+    sparsified_tensor.to_dense()
+
+    return sparsified_tensor.get_future()
+    # return default_hooks._allreduce_fut(process_group, sparsified_tensor)
+
+
+class GradientQuantizationState:
+    def __init__(self, process_group, quant_bits):
+        self.process_group = process_group
+        self.quant_bits = quant_bits
+
+    def quantize(self, tensor):
+        max_val = tensor.abs().max()
+        scale = max_val / (2**self.quant_bits - 1)
+        zero_point = 0
+        quant_tensor = torch.round(tensor / scale) + zero_point
+        quant_tensor = torch.clamp(quant_tensor, 0, 255).to(torch.uint8)
+        return quant_tensor, scale
+
+    def dequantize(quant_tensor, scale):
+        zero_point = 0
+        tensor = scale * (quant_tensor.to(torch.float32) - zero_point)
+        return tensor
+
+def gradient_quantization_hook(state, bucket):
+
+    process_group = state.process_group
+    tensor = bucket.buffer()
+    quant_tensor, scale = state.quantize(tensor)
+    dist.all_reduce(quant_tensor, op=dist.ReduceOp.AVG,
+                    group=process_group)
+    dist.all_reduce(scale, op=dist.ReduceOp.AVG,
+                    group=process_group)
+    dequant_tensor = state.dequantize(quant_tensor, scale)
+
+
+    return dequant_tensor.get_future()
+
+
+##==============================================================================
 
 
 def main(rank, world_size):
@@ -93,6 +174,8 @@ def main(rank, world_size):
         world_size=world_size,
         rank=rank,
     )
+
+    torch.cuda.set_device(rank)
 
     train_loader, train_sampler, test_loader = get_data_stuffs()
     dist_model, model = get_model_stuffs(rank)
@@ -113,9 +196,14 @@ def main(rank, world_size):
 
         dist_model.register_comm_hook(powersgd_state, powersgd_hook)
 
+    elif Method_Work == "topk":
+        topk_state = TopKSparsificationState(process_group, 10)
+        dist_model.register_comm_hook(topk_state, topk_sparsification_hook)
+
     elif Method_Work == "quantize":
-        quant_hook  = quantization_hooks.quantization_perchannel_hook
-        dist_model.register_comm_hook(process_group, quant_hook)
+        quant_state = GradientQuantizationState(process_group, 16)
+        dist_model.register_comm_hook(quant_state, gradient_quantization_hook)
+
 
 
     # Training loop
@@ -144,7 +232,7 @@ def main(rank, world_size):
 
     ## Logging
     utils.LOG2TXT(f'GPU {rank}<-{rank+Rank_Adder} :: Time taken: {time.time()-st_time} \n'  +\
-                f'Loss Start:{loss_list[0]} End:{loss_list[-1]}' +\
+                f'Loss Start:{loss_list[0]} End:{loss_list[-1]} \n' +\
                 f'Steps taken: {step_count}'
                 ,
                 file_path=log_path+"/logs.txt")
